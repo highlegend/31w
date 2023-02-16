cb69509891d700f3f6c0e00afedb848a04fe1cea
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-02-16 16:46:21 -0500

    c6 importer md et modifier page d'accueil

--- front-page.php
+++ front-page.php
@@ -5,19 +5,22 @@
 get_header(); ?>
 <main>
     <h3>Mes articles</h3>
+    <section class="blocflex">
     <?php 
     if (have_posts()):
         while (have_posts()) : the_post();
             // the_title('<h1>','</h1>');
             // the_permalink(); ?>
-          
-            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-            <!-- the_content();    -->
-            <!-- the_excerpt();   /* Permet d'afficher un résumé (le petit extrait qu'on retrouve à droite sur wordpress)  -->
-            <?php echo wp_trim_words(get_the_excerpt(),4);  //get_the_excerpt va retourner une chaîne de caractères
-        endwhile;
-    endif;
-    ?>    
+
+            <article>
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <!-- the_content();    -->
+                <!-- the_excerpt();   /* Permet d'afficher un résumé (le petit extrait qu'on retrouve à droite sur wordpress)  -->
+                <p><?php echo wp_trim_words(get_the_excerpt(),4);   //get_the_excerpt va retourner une chaîne de caractères ?></p>
+            </article>
+        <?php endwhile; ?> 
+     <?php endif;?>    
 </main> 
+    </section>
 <?php get_footer(); ?>
 
