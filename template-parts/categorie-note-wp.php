39ae6a6250a2c329d8914fb2b8cd67df41a3d5d8
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-03-02 16:00:16 -0500

    modif dans le fichier template-parts/categorie-note-wp.hp pour enlever les 0 sur les debuts de titre

--- template-parts/categorie-note-wp.php
+++ template-parts/categorie-note-wp.php
@@ -4,7 +4,8 @@
 *un article provenant d'un conteneur de class blocflex
 *pour un article de categorie note-wp
 */
-$titre = get_the_title();
+$titre =substr( get_the_title(),3);
+//$titre = get_the_title();
 //if(substr($titre,0,1) == "0")   /* dans le cas ou on veux enlever le premier caractere si il y a un 0 dans le debut du titre */
 ?>
 
