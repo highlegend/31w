848e5acaca857dae0feedeed4a7cc65ffa984c67
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-03-15 10:01:00 -0400

    c13 modifier menu cours pour adaptatif

--- header.php
+++ header.php
@@ -4,27 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Entete</title>
+    <title>Document</title>
     <?php wp_head(); ?>
 </head>
-<body class="site <? (is_front_page()?"no-aside":""); ?>">
+<body class="site <?= (is_front_page() ? "no-aside" : "");  ?> ">
     <header class="site__entete">  
         <section class="logomenu">
             <?php the_custom_logo(); ?> 
-            <?php wp_nav_menu(array(
-                'menu' => 'entete',
-                'container' => 'nav'
-            )); ?> 
-            <?= get_search_form(); ?>    
+            <div class="menusearch">
+                <?php wp_nav_menu(array(
+                    'menu' => 'entete',
+                    'container' => 'nav'
+                )); ?> 
+                <?= get_search_form(); ?>   
+            </div> 
         </section>
-        <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1>  <!-- H1 avec un lien a l'inter -->
+        <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
         <h2><?php  bloginfo('description'); ?></h2>
     </header>
-<?php
-if (is_front_page() == false)
-{
-  get_template_part("template-parts/aside");
-}
+<?php 
+    if (is_front_page() == false)
+    {
+        get_template_part("template-parts/aside");
+    }
 ?>
+    
 
 <!-- Le bloginfo est une fonction de WordPress qui permet de récupérer des informations sur le site Web et de les afficher dans des templates ou des articles. Cette fonction peut être utilisée pour afficher des informations telles que le nom du site, l'URL du site, la description du site, l'adresse e-mail de l'administrateur du site, entre autres. -->
