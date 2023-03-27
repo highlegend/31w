b2483442747a3ac70d70f2a4749746209f38e2f4
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-03-27 15:11:36 -0400

    Ajout de la condition de chargement des templates dans single-cours.php et single-notes-cours.php au niveau de single.php

--- single.php
+++ single.php
@@ -7,5 +7,18 @@
 get_header() ?>
 <main class="site__main">
     <h3>index.php</h3>
+    <?php 
+    if (have_posts()):
+   
+   
+     if(in_category('cours')){  
+        get_template_part("template-parts/single", "cours"); 
+     }  
+     else{
+         get_template_part("template-parts/single", "notes-cours"); 
+     }
+     
+    endif;
+?>
 </main> 
 <?php get_footer(); ?>
