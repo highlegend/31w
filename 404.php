c526d2b1b6aa5b89f92b51fc9d4979b2cb4c7eb1
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-04-18 14:40:28 -0400

    Exam final EF- Modification du mondele 404.php pour afficher les pages 404 non trouves

--- 404.php
+++ 404.php
@@ -1,4 +1,7 @@
-<?php
+
+
+
+<!-- <?php
 /**
     Modèle index.php représente le modèle par défaut du thème
 */
@@ -7,4 +10,4 @@ get_header() ?>
     <code>404.php</code>
     <h1>Erreur 404 - la page n'existe pas</h1>
 </main> 
-<?php get_footer(); ?>
+<?php get_footer(); ?> -->
